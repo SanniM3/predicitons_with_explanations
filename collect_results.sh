140ae0be468e552aaf51842e21d2388621ce83c6
Mardhiyah Sanni <43521418+SanniM3@users.noreply.github.com>
2023-06-09 22:58:54 +0100

    Create collect_results.sh

---
+++ collect_results.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+#SBATCH --partition=ILCC_GPU
+#SBATCH --gres=gpu:1
+#SBATCH --mem=32000  # memory in Mb
+#SBATCH --time=0-20:00:00
+
+export CUDA_HOME=/opt/cuda-9.0.176.1/
+
+export CUDNN_HOME=/opt/cuDNN-7.0/
+
+export STUDENT_ID=$(whoami)
+
+export LD_LIBRARY_PATH=${CUDNN_HOME}/lib64:${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
+
+export LIBRARY_PATH=${CUDNN_HOME}/lib64:$LIBRARY_PATH
+
+export CPATH=${CUDNN_HOME}/include:$CPATH
+
+export PATH=${CUDA_HOME}/bin:${PATH}
+
+export PYTHON_PATH=$PATH
+
+# mkdir -p /disk/scratch/${STUDENT_ID}
+
+
+# export TMPDIR=/disk/scratch/${STUDENT_ID}/
+# export TMP=/disk/scratch/${STUDENT_ID}/
+
+# mkdir -p ${TMP}/datasets/
+# export DATASET_DIR=${TMP}/datasets/
+# Activate the relevant virtual environment:
+
+source /home/${STUDENT_ID}/miniconda3/bin/activate feb
+# cd ..
+# python train_evaluate_emnist_classification_system.py --filepath_to_arguments_json_file experiment_configs/emnist_tutorial_config.json
+# #!/usr/bin/env bash
+# conda activate feb
+python scripts/exp.py --exp_root checkpoints --collect_results
