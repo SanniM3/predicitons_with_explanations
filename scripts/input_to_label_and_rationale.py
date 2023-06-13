db0ea9a1ef5621f4d0a9e0d816a1ef11caf970e4
SanniM3 <sannimardhiyah@gmail.com>
2023-06-14 00:42:23 +0100

    reverted to original data path

--- scripts/input_to_label_and_rationale.py
+++ scripts/input_to_label_and_rationale.py
@@ -391,7 +391,8 @@ def main():
             # print(os.getcwd())
             # print(data_args.data_path)
             # cwd, 'data', os.path.basename(data_args)
-            data_path = os.path.join(os.getcwd(), data_args.data_path.lstrip('../'), f"SBIC.v2.{split}.modified.csv")
+            # data_path = os.path.join(os.getcwd(), data_args.data_path.lstrip('../'), f"SBIC.v2.{split}.modified.csv")
+            data_path = os.path.join(os.getcwd(), data_args.data_path, f"SBIC.v2.{split}.modified.csv")
             # print(data_path)
             df = pd.read_csv(data_path)
 
