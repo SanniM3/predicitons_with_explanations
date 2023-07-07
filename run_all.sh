1c69886708f9be3dbd337f72c4eaad745add8898
SanniM3 <sannimardhiyah@gmail.com>
2023-07-07 14:43:56 +0100

    reduced num gpus

--- run_all.sh
+++ run_all.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #SBATCH --partition=ILCC_GPU
 #SBATCH --cpus-per-task=8
-#SBATCH --gres=gpu:8
+#SBATCH --gres=gpu:4
 #SBATCH --mem=64000  # memory in Mb
 #SBATCH --time=0-100:00:00
 #SBATCH --array=1-11
@@ -89,8 +89,8 @@ source /home/${STUDENT_ID}/miniconda3/bin/activate feb
 # #!/usr/bin/env bash
 # conda activate feb
 echo "Your script for ${FULL_JOB_ID}th job has started running" | mail -s "Script Starting Alert" s2421110@ed.ac.uk
-python scripts/exp.py --exp_root checkpoints --not_dryrun --model_vals allenai/unifiedqa-t5-base,allenai/unifiedqa-t5-large,allenai/unifiedqa-t5-3b --dataset_vals ecqa,sensemaking,sbic --n_gpus 8 --lora_rank $current_rank --lora_target_modules "${current_module[@]}"
-python scripts/exp.py --exp_root checkpoints --not_dryrun --model_vals t5-base,t5-large,t5-3b --dataset_vals esnli --n_gpus 8 --lora_rank $current_rank --lora_target_modules "${current_module[@]}"
+python scripts/exp.py --exp_root checkpoints --not_dryrun --model_vals allenai/unifiedqa-t5-base,allenai/unifiedqa-t5-large,allenai/unifiedqa-t5-3b --dataset_vals ecqa,sensemaking,sbic --n_gpus 4 --lora_rank $current_rank --lora_target_modules "${current_module[@]}"
+python scripts/exp.py --exp_root checkpoints --not_dryrun --model_vals t5-base,t5-large,t5-3b --dataset_vals esnli --n_gpus 4 --lora_rank $current_rank --lora_target_modules "${current_module[@]}"
 python scripts/exp.py --exp_root checkpoints --collect_results --lora_rank $current_rank --lora_target_modules "${current_module[@]}"
 # send notification of completion
 echo "Your script for ${FULL_JOB_ID}th job has completed running" | mail -s "Script Completion Alert" s2421110@ed.ac.uk
