cf86ba6fb8d83819b50f8707579d531fa027e96f
SanniM3 <sannimardhiyah@gmail.com>
2023-07-23 21:46:52 +0100

    [refix_tuning rerun of failed seeds

--- run_all.sh
+++ run_all.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:8
 #SBATCH --mem=128000  # memory in Mb
 #SBATCH --time=0-100:00:00
-#SBATCH --array=1-10
+#SBATCH --array=1-5
 
 
 # Get the current array index
@@ -48,8 +48,8 @@ source /home/${STUDENT_ID}/miniconda3/bin/activate feb
 # #!/usr/bin/env bash
 # conda activate feb
 echo "Your script for ${FULL_JOB_ID}th job has started running" | mail -s "Script Starting Alert" s2421110@ed.ac.uk
-python scripts/exp.py --exp_root checkpoints --not_dryrun --model_vals allenai/unifiedqa-t5-base,allenai/unifiedqa-t5-large,allenai/unifiedqa-t5-3b --dataset_vals ecqa,sensemaking,sbic --n_gpus 8 --virtual_tokens $n_current_token
-python scripts/exp.py --exp_root checkpoints --not_dryrun --model_vals t5-base,t5-large,t5-3b --dataset_vals esnli --n_gpus 8 --virtual_tokens $n_current_token
+python scripts/exp.py --exp_root checkpoints --not_dryrun --model_vals allenai/unifiedqa-t5-base,allenai/unifiedqa-t5-large --dataset_vals sbic,sensemaking,ecqa --n_gpus 8 --virtual_tokens $n_current_token
+python scripts/exp.py --exp_root checkpoints --not_dryrun --model_vals t5-base,t5-large --dataset_vals esnli --n_gpus 8 --virtual_tokens $n_current_token
 python scripts/exp.py --exp_root checkpoints --collect_results --virtual_tokens $n_current_token
 # send notification of completion
 echo "Your script for ${FULL_JOB_ID}th job has completed running" | mail -s "Script Completion Alert" s2421110@ed.ac.uk
