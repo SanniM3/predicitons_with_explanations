ebf562807352e43b488d261c432c194724ad8b84
SanniM3 <sannimardhiyah@gmail.com>
2023-07-25 19:05:25 +0100

    remove array job

--- run_all.sh
+++ run_all.sh
@@ -4,15 +4,15 @@
 #SBATCH --gres=gpu:8
 #SBATCH --mem=128000  # memory in Mb
 #SBATCH --time=0-100:00:00
-#SBATCH --array=1-5
 
 
 # Get the current array index
-CURRENT_INDEX=$SLURM_ARRAY_TASK_ID
-FULL_JOB_ID="${SLURM_JOB_ID}_${SLURM_ARRAY_TASK_ID}"
+# CURRENT_INDEX=$SLURM_ARRAY_TASK_ID
+# FULL_JOB_ID="${SLURM_JOB_ID}_${SLURM_ARRAY_TASK_ID}"
+FULL_JOB_ID = $SLURM_JOB_ID
 
 # Set the default parameter values
-n_current_token=$((CURRENT_INDEX * 10))
+# n_current_token=$((CURRENT_INDEX * 10))
 
 
 
@@ -48,8 +48,8 @@ source /home/${STUDENT_ID}/miniconda3/bin/activate feb
 # #!/usr/bin/env bash
 # conda activate feb
 echo "Your script for ${FULL_JOB_ID}th job has started running" | mail -s "Script Starting Alert" s2421110@ed.ac.uk
-python scripts/exp.py --exp_root checkpoints --not_dryrun --model_vals allenai/unifiedqa-t5-base,allenai/unifiedqa-t5-large --dataset_vals sbic,sensemaking,ecqa --n_gpus 8 --virtual_tokens $n_current_token
-python scripts/exp.py --exp_root checkpoints --not_dryrun --model_vals t5-base,t5-large --dataset_vals esnli --n_gpus 8 --virtual_tokens $n_current_token
-python scripts/exp.py --exp_root checkpoints --collect_results --virtual_tokens $n_current_token
+python scripts/exp.py --exp_root checkpoints --not_dryrun --model_vals allenai/unifiedqa-t5-base,allenai/unifiedqa-t5-large --dataset_vals sbic,sensemaking,ecqa --n_gpus 8 --virtual_tokens $10
+python scripts/exp.py --exp_root checkpoints --not_dryrun --model_vals t5-base,t5-large --dataset_vals esnli --n_gpus 8 --virtual_tokens $10
+python scripts/exp.py --exp_root checkpoints --collect_results --virtual_tokens $10
 # send notification of completion
 echo "Your script for ${FULL_JOB_ID}th job has completed running" | mail -s "Script Completion Alert" s2421110@ed.ac.uk
