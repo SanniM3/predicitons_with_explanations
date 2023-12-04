6ad5adda778e6f6a3a8f8fb703ed8848e78dfe10
SanniM3 <sannimardhiyah@gmail.com>
2023-12-04 07:59:18 +0000

    debug removed t5 collator

--- scripts/input_to_label_and_rationale.py
+++ scripts/input_to_label_and_rationale.py
@@ -556,14 +556,21 @@ def main():
         training_args.bf16=True
         training_args.bf16_full_eval=True
 
+        # trainer = Trainer(
+        #     model=model,
+        #     args=training_args,
+        #     train_dataset=data_splits['train'],
+        #     eval_dataset=data_splits['validation'],
+        #     data_collator=SequenceCollator(
+        #         model=model_class, pad_token=tokenizer.pad_token_id
+        #     ),
+        #     callbacks=callbacks,
+        # )
         trainer = Trainer(
             model=model,
             args=training_args,
             train_dataset=data_splits['train'],
             eval_dataset=data_splits['validation'],
-            data_collator=SequenceCollator(
-                model=model_class, pad_token=tokenizer.pad_token_id
-            ),
             callbacks=callbacks,
         )
 
