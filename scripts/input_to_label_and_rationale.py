9b3b7c46b5fa13e1b786466ae5ca2a73a2a19968
Mardhiyah Sanni <43521418+SanniM3@users.noreply.github.com>
2024-03-30 10:23:45 +0000

    set all params to require grad

--- scripts/input_to_label_and_rationale.py
+++ scripts/input_to_label_and_rationale.py
@@ -580,12 +580,12 @@ def main():
         # model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf", token='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')
 
         #SPARSEFIT CHANGES
-        # # Make trainable only key terms in self-attention layers.
-        # # if 'attention.k' in model_args.bias_terms:
-        # for param in model.parameters():
-        #     param.requires_grad = False
-        # # Deactivate language model head
-        # model.lm_head.weight.requires_grad = False
+        # Make trainable only key terms in self-attention layers.
+        # if 'attention.k' in model_args.bias_terms:
+        for param in model.parameters():
+            param.requires_grad = True
+        # Activate language model head
+        model.lm_head.weight.requires_grad = True
 
         # for name, param in model.named_parameters():
         #     if 'self_attn.q_proj' in name:
